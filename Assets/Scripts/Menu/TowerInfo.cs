288101a708cb269a3f2606cbdfcca6ef5c7231b6
MarvinKnobloch <NivramKnobloch@gmail.com>
2025-10-05 01:37:30 +0200

    TowerInfo

---
+++ Assets/Scripts/Menu/TowerInfo.cs
@@ -0,0 +1,13 @@
+using TMPro;
+using UnityEngine;
+
+public class TowerInfo : MonoBehaviour
+{
+    [SerializeField] private GameObject InfoMenu;
+    [SerializeField] private TextMeshProUGUI numberText;
+
+    public void TowerInfoUpdate()
+    {
+        
+    }
+}
