8d991360ee92b05ed58b8d06c7679d9dccc86af5
MarvinKnobloch <NivramKnobloch@gmail.com>
2025-10-05 16:27:01 +0200

    Drag Image

--- Assets/Scripts/Inventory/InventorySlot.cs
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -1,19 +1,26 @@
 using TMPro;
+using Tower;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class InventorySlot : MonoBehaviour
+public class InventorySlot : MonoBehaviour, IPointerClickHandler
 {
+    [HideInInspector] public Inventory inventory;
     public int slotAmount;
+    public BodyObject bodyObject;
+
+
     [SerializeField] private TextMeshProUGUI amountText;
     private Image inventoryImage;
+
 
     private void Awake()
     {
         inventoryImage = GetComponent<Image>();
     }
 
-    public void SetAmount(int amount, Sprite icon)
+    public void SetValues(int amount, Sprite icon)
     {
         slotAmount = amount;
         if (amount > 0)
@@ -31,5 +38,12 @@ public class InventorySlot : MonoBehaviour
     public void HideText()
     {
         amountText.gameObject.SetActive(false);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (slotAmount == 0 || bodyObject == null) return;
+
+        inventory.StartDrag(bodyObject);
     }
 }
