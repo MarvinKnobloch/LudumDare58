b14cce118f679facdcb10b4043fe027b8a22b1dd
dese2304 <desireeott2304@gmail.com>
2025-10-07 20:20:10 +0200

    Drops spawnen jz mit weniger chancen
    muss noch gebalanced und bestimmte müssen eingefügt werden

--- Assets/Scripts/Enemies/EnemyDrop.cs
+++ Assets/Scripts/Enemies/EnemyDrop.cs
@@ -1,30 +1,35 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyDrop : MonoBehaviour
 {
     [SerializeField] private DropValues[] dropValues;
-    [SerializeField] private float scatterRadius = 8f;  
+    [SerializeField] private float scatterRadius = 8f;
     [SerializeField] private bool addForce = true;
 
     public void DropItems(Vector3 position)
     {
         for (int i = 0; i < dropValues.Length; i++)
         {
-            if (dropValues[i].item == null)
+            var drop = dropValues[i];
+
+            if (drop.item == null)
                 continue;
 
-            int amountToDrop = (i < dropValues.Length) ? dropValues[i].amount : 1;
+            if (Random.value > drop.dropChance)
+                continue;
+
+            int amountToDrop = drop.amount;
 
             for (int j = 0; j < amountToDrop; j++)
             {
                 Vector2 randomDirection = Random.insideUnitCircle.normalized * Random.Range(3f, scatterRadius);
                 Vector3 dropPosition = position + new Vector3(randomDirection.x, randomDirection.y, 0f);
 
-                GameObject drop = Instantiate(dropValues[i].item, dropPosition, Quaternion.identity);
+                GameObject newDrop = Instantiate(drop.item, dropPosition, Quaternion.identity);
 
                 if (addForce)
                 {
-                    Rigidbody2D rb = drop.GetComponent<Rigidbody2D>();
+                    Rigidbody2D rb = newDrop.GetComponent<Rigidbody2D>();
                     if (rb != null)
                     {
                         Vector2 randomForce = randomDirection * Random.Range(8f, 16f);
@@ -36,14 +41,10 @@ public class EnemyDrop : MonoBehaviour
     }
 }
 
-
 [System.Serializable]
 public class DropValues
 {
-    public GameObject item;   
-    
-    public float dropChance;  
-
-    public int amount;        
+    public GameObject item;
+    [Range(0f, 1f)] public float dropChance = 0.5f;  // z.B. 0.5 = 50%
+    public int amount = 1;
 }
-
