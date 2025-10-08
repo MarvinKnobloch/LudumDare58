32c01c7810051516c40d19d92043b129ac0420e4
dese2304 <desireeott2304@gmail.com>
2025-10-08 20:55:25 +0200

    Drops

--- Assets/Scripts/Enemies/EnemyDrop.cs
+++ Assets/Scripts/Enemies/EnemyDrop.cs
@@ -6,6 +6,9 @@ public class EnemyDrop : MonoBehaviour
     [SerializeField] private float scatterRadius = 8f;
     [SerializeField] private bool addForce = true;
 
+    [SerializeField] private Collider2D buildArea; 
+
+
     public void DropItems(Vector3 position)
     {
         for (int i = 0; i < dropValues.Length; i++)
@@ -32,12 +35,35 @@ public class EnemyDrop : MonoBehaviour
                     Rigidbody2D rb = newDrop.GetComponent<Rigidbody2D>();
                     if (rb != null)
                     {
-                        Vector2 randomForce = randomDirection * Random.Range(8f, 16f);
+                        Vector2 randomForce = Random.insideUnitCircle.normalized * Random.Range(3f, 6f);
                         rb.AddForce(randomForce, ForceMode2D.Impulse);
+
+                        float randomTorque = Random.Range(-6, 6f); //Drehung
+                        rb.AddTorque(randomTorque, ForceMode2D.Impulse);
+
+                        rb.linearDamping = Random.Range(1f, 3f);   //Luftwiderstand
+                        rb.angularDamping = Random.Range(1f, 2f); //Rotationswiderstand
+                        
+
+                        
                     }
                 }
             }
         }
+    }
+
+    public Vector3 GetRandomPositionInsideBuildArea()
+    {
+
+
+        Collider2D buildArea = BuildAreaManager.Instance.AreaCollider;
+
+        Bounds bounds = buildArea.bounds;
+
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+
+        return new Vector3(x, y, 0f);
     }
 }
 
@@ -45,6 +71,6 @@ public class EnemyDrop : MonoBehaviour
 public class DropValues
 {
     public GameObject item;
-    [Range(0f, 1f)] public float dropChance = 0.5f;  // z.B. 0.5 = 50%
+    [Range(0f, 1f)] public float dropChance = 0.75f; 
     public int amount = 1;
 }
